e5963b9c583b5af7555b0650a6b96d2393e4c0e3
youssef khemiri <youssefkhemiri722@gmail.com>
2022-06-17 14:22:28 +0100

    express gen REST API

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
 var app = express();
 
 // view engine setup
@@ -18,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
