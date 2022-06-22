dd2fed412bb506f907e9ba63527d6b28116515b8
youssef khemiri <youssefkhemiri722@gmail.com>
2022-06-22 16:14:19 +0100

    auth

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const mongoose = require('mongoose');
 // const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/ConFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url,{ useNewUrlParser: true });
 
 connect.then((db) => {
     console.log("Connected correctly to server");
@@ -48,7 +48,29 @@ app.use(session({
 }));
 
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
+
+function auth (req, res, next) {
+  console.log(req.session);
+
+if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+}
+else {
+  if (req.session.user === 'authenticated') {
+    next();
+  }
+  else {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+}
+}
 
 // function auth (req, res, next) {
 
@@ -63,7 +85,8 @@ app.use(session({
   //     return;
   // }
 
-  // var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  //
+  //  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
   // var user = auth[0];
   // var pass = auth[1];
   // if (user == 'admin' && pass == 'password') {
@@ -111,43 +134,43 @@ app.use(session({
 //   }
 // }
 
-function auth (req, res, next) {
-  console.log(req.session);
+// function auth (req, res, next) {
+//   console.log(req.session);
 
-  if (!req.session.user) {
-      var authHeader = req.headers.authorization;
-      if (!authHeader) {
-          var err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');                        
-          err.status = 401;
-          next(err);
-          return;
-      }
-      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      var user = auth[0];
-      var pass = auth[1];
-      if (user == 'admin' && pass == 'password') {
-          req.session.user = 'admin';
-          next(); // authorized
-      } else {
-          var err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          next(err);
-      }
-  }
-  else {
-      if (req.session.user === 'admin') {
-          console.log('req.session: ',req.session);
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
-  }
-}
+//   if (!req.session.user) {
+//       var authHeader = req.headers.authorization;
+//       if (!authHeader) {
+//           var err = new Error('You are not authenticated!');
+//           res.setHeader('WWW-Authenticate', 'Basic');                        
+//           err.status = 401;
+//           next(err);
+//           return;
+//       }
+//       var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//       var user = auth[0];
+//       var pass = auth[1];
+//       if (user == 'admin' && pass == 'password') {
+//           req.session.user = 'admin';
+//           next(); // authorized
+//       } else {
+//           var err = new Error('You are not authenticated!');
+//           res.setHeader('WWW-Authenticate', 'Basic');
+//           err.status = 401;
+//           next(err);
+//       }
+//   }
+//   else {
+//       if (req.session.user === 'admin') {
+//           console.log('req.session: ',req.session);
+//           next();
+//       }
+//       else {
+//           var err = new Error('You are not authenticated!');
+//           err.status = 401;
+//           next(err);
+//       }
+//   }
+// }
 
 
 app.use(auth);
@@ -159,8 +182,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
